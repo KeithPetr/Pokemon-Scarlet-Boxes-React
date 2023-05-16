c8af666a1f7fc520f02f836ed556131c8237fef9
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-05-16 19:40:01 -0300

    finished basic CSS for Party inner component

--- src/components/PartyComponent.jsx
+++ src/components/PartyComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PartyComponent() {
   return (
     <div className="party-component">
-      <div>
+      <div className="party-details">
         <div className="party-poke-level">Lv. 100</div>
         <div className="party-poke-name">Dragonite</div>
       </div>
