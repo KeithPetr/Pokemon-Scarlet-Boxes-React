0b74020636e99847efbdbb5012f6b3a795fe0454
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-05-16 16:05:27 -0300

    Finished a similar layout to screenshot for Header using CSS

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
 
 export default function Header() {
-    return (
-        <h1>Header</h1>
-    )
+  return (
+    <header>
+      <ul className="nav-bar-top">
+      <div className="x-border-right"><li className="x-button">X</li></div>
+        <li className="nav-party">Party and Boxes</li>
+        <li className="nav-held">Held Items</li>
+        <li className="nav-battle">Battle Teams</li>
+      </ul>
+    </header>
+  );
 }
