3568a00dd4840cda92da2ac81e7da29468c6b863
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-05-25 17:49:36 -0300

    Able to add correct colors and icons to types

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,9 +2,25 @@ import React, { useContext } from "react";
 import UltraBall from "../PokemonIcons/ultra-ball.png";
 import Male from "../PokemonIcons/male.png";
 import Revive from "../PokemonIcons/revive.png";
-import Fire from "../PokemonIcons/fire.svg";
-import Psychic from "../PokemonIcons/psychic.svg";
-import Ghost from "../PokemonIcons/ghost.svg";
+import fire from "../PokemonIcons/fire.svg";
+import psychic from "../PokemonIcons/psychic.svg";
+import bug from "../PokemonIcons/bug.svg";
+import dark from "../PokemonIcons/dark.svg";
+import dragon from "../PokemonIcons/dragon.svg";
+import electric from "../PokemonIcons/electric.svg";
+import fighting from "../PokemonIcons/fighting.svg";
+import fairy from "../PokemonIcons/fairy.svg";
+import flying from "../PokemonIcons/flying.svg";
+import ghost from "../PokemonIcons/ghost.svg";
+import grass from "../PokemonIcons/grass.svg";
+import ground from "../PokemonIcons/ground.svg";
+import ice from "../PokemonIcons/ice.svg";
+import normal from "../PokemonIcons/normal.svg";
+import poison from "../PokemonIcons/poison.svg";
+import rock from "../PokemonIcons/rock.svg";
+import steel from "../PokemonIcons/steel.svg";
+import water from "../PokemonIcons/water.svg";
+
 import Circle from "../ShapeIcons/circle.png";
 import Heart from "../ShapeIcons/heart.png";
 import Kite from "../ShapeIcons/kite.png";
@@ -18,7 +34,6 @@ export default function Stats() {
   console.log(selectedPokemon);
 
   if (!selectedPokemon) {
-    // Render blank elements or a loading indicator
     return (
       <div className="stats">
         <h1>Blank</h1>
@@ -26,16 +41,120 @@ export default function Stats() {
     );
   }
 
-  function typeTwo() {
+  function getTypeStyle(type) {
+    let backgroundColor = "";
+    let typeIcon = "";
+
+    if (type === "psychic") {
+      backgroundColor = "pink";
+      typeIcon = psychic;
+    } else if (type === "grass") {
+      backgroundColor = "green";
+      typeIcon = grass;
+    } else if (type === "bug") {
+      backgroundColor = "yellowgreen";
+      typeIcon = bug;
+    } else if (type === "fire") {
+      backgroundColor = "red";
+      typeIcon = fire;
+    } else if (type === "water") {
+      backgroundColor = "blue";
+      typeIcon = water;
+    } else if (type === "dark") {
+      backgroundColor = "purple";
+      typeIcon = dark;
+    } else if (type === "dragon") {
+      backgroundColor = "orange";
+      typeIcon = dragon;
+    } else if (type === "electric") {
+      backgroundColor = "yellow";
+      typeIcon = electric;
+    } else if (type === "fairy") {
+      backgroundColor = "palevioletred";
+      typeIcon = fairy;
+    } else if (type === "fighting") {
+      backgroundColor = "burlywood";
+      typeIcon = fighting;
+    } else if (type === "flying") {
+      backgroundColor = "whitesmoke";
+      typeIcon = flying;
+    } else if (type === "ground") {
+      backgroundColor = "brown";
+      typeIcon = ground;
+    } else if (type === "ice") {
+      backgroundColor = "lightblue";
+      typeIcon = ice;
+    } else if (type === "normal") {
+      backgroundColor = "seashell";
+      typeIcon = normal;
+    } else if (type === "poison") {
+      backgroundColor = "plum";
+      typeIcon = poison;
+    } else if (type === "rock") {
+      backgroundColor = "gray";
+      typeIcon = rock;
+    } else if (type === "steel") {
+      backgroundColor = "silver";
+      typeIcon = steel;
+    } else if (type === "ghost") {
+      backgroundColor = "darkmagenta";
+      typeIcon = ghost;
+    }
+
+    return {
+      backgroundColor,
+      typeIcon,
+    };
+  }
+
+  function renderTypes() {
+    const typeOne = selectedPokemon.types[0].type.name;
+    const typeOneStyle = getTypeStyle(typeOne);
+
     if (selectedPokemon.types.length > 1) {
+      const typeTwo = selectedPokemon.types[1].type.name;
+      const typeTwoStyle = getTypeStyle(typeTwo);
+
       return (
-        <div className="type">
-          <img className="type-icon" src={Psychic} />
-          <div className="type-name">{selectedPokemon.types[1].type.name}</div>
-        </div>
+        <>
+          <div
+            className="type"
+            style={{ backgroundColor: typeOneStyle.backgroundColor }}
+          >
+            <img
+              className="type-icon"
+              src={typeOneStyle.typeIcon}
+              alt={typeOne}
+            />
+            <div className="type-name">{typeOne}</div>
+          </div>
+          <div
+            className="type"
+            style={{ backgroundColor: typeTwoStyle.backgroundColor }}
+          >
+            <img
+              className="type-icon"
+              src={typeTwoStyle.typeIcon}
+              alt={typeTwo}
+            />
+            <div className="type-name">{typeTwo}</div>
+          </div>
+        </>
       );
     } else {
-      return null;
+      return (
+        <div
+          className="type"
+          style={{ backgroundColor: typeOneStyle.backgroundColor }}
+        >
+          <img
+            className="type-icon"
+            src={typeOneStyle.typeIcon}
+            alt={typeOne}
+          />
+          <div className="type-name">{typeOne}</div>
+        </div>
+      );
     }
   }
 
@@ -56,17 +175,7 @@ export default function Stats() {
         <h3>{selectedPokemon.species.name}</h3>
         <img className="revive" src={Revive} />
       </div>
-      <div className="type-header">
-        <div className="type">
-          <img className="type-icon" src={Fire} />
-          <div className="type-name">{selectedPokemon.types[0].type.name}</div>
-        </div>
-        {typeTwo()}
-        <div className="type">
-          <img className="type-icon" src={Ghost} />
-          <div className="type-name">Ghost</div>
-        </div>
-      </div>
+      <div className="type-header">{renderTypes()}</div>
       <div className="base-stats">
         <div className="hp">
           <div>
@@ -110,19 +219,19 @@ export default function Stats() {
       </div>
       <div className="move-list">
         <div className="move">
-          <img className="move-icon" src={Fire} />
+          <img className="move-icon" src={fire} />
           <div className="move-name">{selectedPokemon.moves[0].move.name}</div>
         </div>
         <div className="move">
-          <img className="move-icon" src={Fire} />
+          <img className="move-icon" src={fire} />
           <div className="move-name">{selectedPokemon.moves[1].move.name}</div>
         </div>
         <div className="move">
-          <img className="move-icon" src={Fire} />
+          <img className="move-icon" src={fire} />
           <div className="move-name">{selectedPokemon.moves[2].move.name}</div>
         </div>
         <div className="move">
-          <img className="move-icon" src={Fire} />
+          <img className="move-icon" src={fire} />
           <div className="move-name">{selectedPokemon.moves[3].move.name}</div>
         </div>
       </div>
