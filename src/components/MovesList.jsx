ab28064bca59761161ff09d208f3b4775d3b4f5e
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-05-26 11:01:52 -0300

    able to get the moves array for the selected pokemon
    When a pokemon is clicked on, the first 4 move names are taken into an array and then mapped over to all the move endpoint using each of those names. The returned move objects are placed into a new array with more detailed move data.

--- src/components/MovesList.jsx
+++ src/components/MovesList.jsx
@@ -4,22 +4,27 @@ import PokemonContext from "../PokemonContext";
 export default function MovesList() {
   const { selectedPokemon } = useContext(PokemonContext);
   const moves = selectedPokemon.moves.slice(0, 4);
-  console.log(moves);
+  const [moveArray, setMoveArray] = useState([]);
 
   useEffect(() => {
     async function moveData() {
       try {
         const movePromises = moves.map(async (move) => {
-          const response = await fetch(`https://pokeapi.co/api/v2/move/${move.move.name}`);
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/move/${move.move.name}`
+          );
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
           const data = await response.json();
           // Handle the fetched move data here
+
           console.log(data);
+          return data;
         });
 
-        await Promise.all(movePromises);
+        const fetchedMoves = await Promise.all(movePromises);
+        setMoveArray(fetchedMoves);
       } catch (error) {
         console.log("Error occurred during fetch request:", error);
       }
@@ -28,7 +33,11 @@ export default function MovesList() {
     if (moves.length > 0) {
       moveData();
     }
-  }, [moves]);
+  }, [selectedPokemon]);
+
+  useEffect(() => {
+    console.log(moveArray);
+  }, [moveArray]);
 
   return null; // Placeholder return statement, adjust as needed
 }
