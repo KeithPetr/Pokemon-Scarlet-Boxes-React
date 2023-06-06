2138a90baa38b52046d67712dc1900b5547b9c7b
KeithPetr <91621041+KeithPetr@users.noreply.github.com>
2023-06-06 17:45:54 -0300

    Can drag an image and get it to appear in the party with its name

--- src/components/Party.jsx
+++ src/components/Party.jsx
@@ -1,19 +1,38 @@
-import React from "react"
-import PartyItem from "./PartyItem"
+import React, { useContext, useState } from "react";
+import PartyItem from "./PartyItem";
+import PokemonContext from "../PokemonContext";
 
 export default function Party() {
-    return (
-        <section className="party">
-            <div className="party-header">
-                <div className="party-pokeball">O</div>
-                <div className="party-current-title">Current Party</div>
-            </div>
-            <PartyItem />
-            <PartyItem />
-            <PartyItem />
-            <PartyItem />
-            <PartyItem />
-            <PartyItem />
-        </section>
-    )
+  const { pokemonData, handleBoxItemClick } = useContext(PokemonContext);
+  const [partyPokemon, setPartyPokemon] = useState([]);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+  
+    // Get the Pokémon data from the dropped data
+    const droppedPokemon = JSON.parse(event.dataTransfer.getData("text/plain"));
+  
+    // Add the dropped Pokémon to the party
+    setPartyPokemon([...partyPokemon, droppedPokemon]);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const renderPartyPokemon = () => {
+    return partyPokemon.map((pokemon) => (
+      <PartyItem key={pokemon.id} pokemon={pokemon} />
+    ));
+  };
+
+  return (
+    <section className="party" onDrop={handleDrop} onDragOver={handleDragOver}>
+      <div className="party-header">
+        <div className="party-pokeball">O</div>
+        <div className="party-current-title">Current Party</div>
+      </div>
+      {renderPartyPokemon()}
+    </section>
+  );
 }
--- src/components/PartyItem.jsx
+++ src/components/PartyItem.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-export default function PartyItem() {
-const imgURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/46.png"
-
+export default function PartyItem({ pokemon }) {
   return (
     <div className="party-component">
       <div className="party-details">
         <div className="poke-level">Lv. 100</div>
-        <div className="party-poke-name">Dragonite</div>
+        <div className="party-poke-name">{pokemon.species.name}</div>
       </div>
-      <img className="party-poke-image" src={imgURL}/>
+      <img className="party-poke-image" src={pokemon.sprites.front_default} alt={pokemon.species.name} />
     </div>
   );
 }
